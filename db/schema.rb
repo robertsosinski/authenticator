67985e1e67ba24bcd601974f0b79edfc53424c24
Robert Sosinski <robert@capansis.com>
2008-07-25 22:31:39 -0400

    Pushing schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080725201455) do
+ActiveRecord::Schema.define(:version => 20080725235607) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email_address"
